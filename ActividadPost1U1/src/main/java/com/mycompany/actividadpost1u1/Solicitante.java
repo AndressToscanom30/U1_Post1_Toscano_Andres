6a1ce3c4c427870eeaf9b515baf5cd1050791e21
Andres Toscano <andress.toscano@gmail.com>
2026-02-23 17:31:03 -0500

    Clase solicitante con PRE, POST e invariantes

--- ActividadPost1U1/src/main/java/com/mycompany/actividadpost1u1/Solicitante.java
+++ ActividadPost1U1/src/main/java/com/mycompany/actividadpost1u1/Solicitante.java
@@ -6,15 +6,16 @@ package com.mycompany.actividadpost1u1;
  */
 public class Solicitante {
 
-    private String nombre;
-    private int edad;
-    private int puntajeCrediticio;
-    private boolean tieneDeudas;
-    private double aniosEmpleo;
-    private double ingresoMensual;
+    private final String nombre;
+    private final int edad;
+    private final int puntajeCrediticio;
+    private final boolean tieneDeudas;
+    private final double aniosEmpleo;
+    private final double ingresoMensual;
 
     public Solicitante(String nombre, int edad, int puntajeCrediticio, boolean tieneDeudas, double aniosEmpleo, double ingresoMensual) {
-        // TODO: Asignar atributos
+
+        //Atributos asignados
         this.nombre = nombre;
         this.edad = edad;
         this.puntajeCrediticio = puntajeCrediticio;
@@ -25,10 +26,44 @@ public class Solicitante {
         verificarInvariante();
     }
 
+    /*Evalúa una solicitud de préstamo.
+    * PRECONDICIONES: 
+       - sol != null 
+       - monto > 0 
+       - sol.getEdad() >= 0 
+       - sol cumple su invariante
+
+    * POSTCONDICIONES: 
+       - Retorna APROBADO si: 
+           puntaje >= 700 
+           && !tieneDeudas 
+           && monto <= 5 * ingreso
+           && edad >= 18
+
+       - Retorna REVISION_MANUAL si: 
+           (puntaje >= 700 && tieneDeudas) 
+           OR (puntaje entre 500 y 699 && empleo >= 2)
+
+       - Retorna RECHAZADO en cualquier otro caso
+   */
     private void verificarInvariante() {
-        // TODO: Implementar invariantes con assert o excepciones
-        if (this.tieneDeudas){
-            
+        if (nombre == null || nombre.isBlank()) {
+            throw new IllegalArgumentException("Nombre inválido");
+        }
+        if (edad <= 0) {
+            throw new IllegalArgumentException("Edad debe ser > 0");
+        }
+        if (puntajeCrediticio < 0 || puntajeCrediticio > 850) {
+            throw new IllegalArgumentException(
+                    "Puntaje debe estar entre 0 y 850");
+        }
+        if (aniosEmpleo < 0) {
+            throw new IllegalArgumentException(
+                    "Años de empleo no pueden ser negativos");
+        }
+        if (ingresoMensual < 0) {
+            throw new IllegalArgumentException(
+                    "Ingreso mensual no puede ser negativo");
         }
     }
 
@@ -56,7 +91,5 @@ public class Solicitante {
     public double getIngresoMensual() {
         return ingresoMensual;
     }
-    
-    
-    
+
 }
